9c9b0c20d2dcef5d3fc12e427c1e92ef3ad5b17a
akelly <alexandrakelly@alexandras-macbook-air-5.local>
2017-04-24 11:44:35 -0500

    tried to add pin annotation

--- Travel Diary/DetailViewController.swift
+++ Travel Diary/DetailViewController.swift
@@ -44,8 +44,11 @@ class DetailViewController: UIViewController, UIImagePickerControllerDelegate, U
         }
         
         @IBAction func onTappedSaveButton(_ sender: UIButton) {
-           CLLocationManager()
-        
+            if locationTextField = ""
+            return
+            findLocation()
+            displayPin() 
+            
         }
     
         
