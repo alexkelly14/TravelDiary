6c82f9db332a33915d0336fef9a9e0bffef7413f
akelly <alexandrakelly@alexandras-macbook-air-5.local>
2017-05-04 11:20:22 -0500

    f

--- Travel Diary/DetailViewController.swift
+++ Travel Diary/DetailViewController.swift
@@ -44,7 +44,7 @@ class DetailViewController: UIViewController, UIImagePickerControllerDelegate, U
         }
         
         @IBAction func onTappedSaveButton(_ sender: UIButton) {
-        navigationController?.popToViewController(<#UIViewController#>, animated: true)
+    //    navigationController?.popToViewController(<#UIViewController#>, animated: true)
                 }
             }
     
